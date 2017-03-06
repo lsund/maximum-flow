1f1f3e4ebfece7fdd162c2ced387fc3cf8474e63
Ludvig Sundström <lud.sund@gmail.com>
2017-03-06 11:57:17 +0100

    checking for unspecified input

--- main.c
+++ main.c
@@ -7,15 +7,17 @@
 #include "goldberg_tarjan.h"
 #include "pseudoflow.h"
 
-char *filename;
+char *file;
+char *outfile;
+int output;
 NetworkType type;
 
-static void print_before(const char *filename, const NetworkType type)
+static void print_before(const char *file, const NetworkType type)
 {
     printf("type 0 = push relabel network\ntype 1 = pseudoflow network\n");
     printf(
             "Computing flow of the network in file %s using network type %d\n", 
-            filename,
+            file,
             type
         );
 }
@@ -28,29 +30,47 @@ static void print_after(const NetworkPointer network, const int msec)
 
 static void parse_arguments(int argc, char *argv[])
 {
+    bool file, specified_type, specified_outfile;
+    file              = false;
+    specified_outfile = false;
+    specified_type    = false;
     int opt;
-    int i = 1;
+    int i   = 1;
     while ((opt = getopt(argc, argv, "fto")) != 1) {
         i++;
         switch (opt) {
             case 'f': 
-                filename = argv[i];
+                file = argv[i];
                 i++;
+                file = true;
                 break;
             case 't': 
                 type = strcmp("pr", argv[i]) == 0 ? PR : PS;
                 i++;
+                type = true;
                 break;
             case 'o': 
-                printf("%s\n", argv[i]);
+                outfile = argv[i];
                 i++;
+                specified_outfile = true;
                 break;
             case -1:
                 return;
             default: 
-                printf("%d\n", opt);
-                fprintf(stderr, "Usage : %s [ilw] \n", argv[0]);
+                fprintf(
+                        stderr, "Usage : %s -f file [-t type] [-o outfile] \n",
+                        argv[0]
+                    );
                 exit(EXIT_FAILURE);
+        }
+        if (!file) {
+            fprintf(
+                    stderr, "Usage : %s -f file [-t type] [-o outfile] \n",
+                    argv[0]
+                );
+        }
+        if (!type) {
+            type = PR;
         }
     }
 }
@@ -58,8 +78,8 @@ static void parse_arguments(int argc, char *argv[])
 int main(int argc, char *argv[]) 
 {
     parse_arguments(argc, argv);
-    NetworkPointer network = parse(filename, type);
-    print_before(filename, type);
+    NetworkPointer network = parse(file, type);
+    print_before(file, type);
     clock_t start = clock(), diff;
     if (network->type == PR) {
         goldberg_tarjan(network);
@@ -68,7 +88,11 @@ int main(int argc, char *argv[])
     }
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
-    print_after(network, msec);
+    if (outfile) {
+        /* print_file(network, outfile); */
+    } else {
+        print_after(network, msec);
+    }
     network_destroy(network);
 
     return 0;
